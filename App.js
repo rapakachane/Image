1b5c5b5fad3ad6f16a90b5e0d1e43a173149f2c9
rapakachane <rapakachanelenka56@gmail.com>
2022-05-18 17:55:49 +0200

    clans

--- App.js
+++ App.js
@@ -1,32 +1,30 @@
 import { StyleSheet, Text, View, StatusBar, TouchableOpacity,Image } from 'react-native';
 import { useState } from 'react';
-import { AntDesign, Ionicons } from '@expo/vector-icons';
 import Images from './Images';
 
 export default function App() {
-  const[image,setImage] = useState(require('./assets/Mmaletsunyane.jpg'));
+  const[image,setImage] = useState(require('./assets/HIPPO BAKUBUNG.jpg'));
 
-    function happyImage(){
-        setImage(require('./assets/Mmaletsunyane.jpg'));
+    function HIPPO(){
+        setImage(require('./assets/HIPPO BAKUBUNG.jpg'));
         
     }
-    function sadImage(){
-        setImage(require('./assets/long.png'));
+    function LION(){
+        setImage(require('./assets/LION BATAUNG.jpg'));
     }
-    function okImage(){
-
+    function TIGER(){
+      setImage(require('./assets/TIGER BATLOKOA.jpg'));
+    }
+    function CAT(){
+      setImage(require('./assets/CAT BASIA.jpg'));
     }
   return (
     <View style={styles.container}>
 
       <View style={styles.header}>
-         <TouchableOpacity>
-          <Ionicons name="arrow-back-circle" size={24} color="#f5f5f5" />
-         </TouchableOpacity>   
-        <Text style={styles.textHeader}>My Gallery</Text>
-        <TouchableOpacity>
-         <AntDesign name="bars" size={24} color="#f5f5f5" />
-        </TouchableOpacity>
+         
+        <Text style={styles.textHeader}>BASOTHO CLANS</Text>
+        
       </View>
 
       <View style={styles.body}>
@@ -36,15 +34,27 @@ export default function App() {
       </View>
       <View style={styles.footer}>
        
-      <TouchableOpacity onPress={happyImage}>
-      <Image style={styles.image} source={require('./assets/Mmaletsunyane.jpg')} resizeMode="cover" />
-      <Text style={styles.text}>Mmaletsunyane</Text> 
+      <TouchableOpacity onPress={HIPPO}>
+      <Image style={styles.image} source={require('./assets/HIPPO BAKUBUNG.jpg')} resizeMode="cover" />
+      <Text style={styles.text}>HIPPOPOTOMUS</Text> 
       </TouchableOpacity>
       
-      <TouchableOpacity onPress={sadImage}>
-        <Image style={styles.image} source={require('./assets/long.png')} />
-        <Text style={styles.text}>Victoria</Text>
+      <TouchableOpacity onPress={LION}>
+        <Image style={styles.image} source={require('./assets/LION BATAUNG.jpg')} />
+        <Text style={styles.text}>LION</Text>
       </TouchableOpacity>
+
+      <TouchableOpacity onPress={CAT}>
+        <Image style={styles.image} source={require('./assets/CAT BASIA.jpg')} />
+        <Text style={styles.text}>CAT</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={TIGER}>
+        <Image style={styles.image} source={require('./assets/TIGER BATLOKOA.jpg')} />
+        <Text style={styles.text}>TIGER</Text>
+      </TouchableOpacity>
+
+      
       </View>
       <StatusBar style="auto" />
     </View>
@@ -55,19 +65,20 @@ const styles = StyleSheet.create({
 
   container: {
     flex: 1,
-    backgroundColor: '#2C3539',
+    backgroundColor: 'green',
     alignItems: 'center',
     justifyContent: 'center',
   },
   textHeader:{
-    fontSize:20,
+    fontSize:28,
     color:'#f2f2f2',
     fontWeight: 'bold',
+    
   },
   header:{
 
-    flexDirection:'row',
-    justifyContent:'space-between',
+    alignContent:'center',
+    alignItems:'center',
     marginTop: 10,
     padding:10,
     width:'100%',
@@ -77,31 +88,27 @@ const styles = StyleSheet.create({
 
     flex:3,
     marginTop:20,
-    width:'90%',
+    width:'100%',
     
     backgroundColor: '#f5f5f5',
-    borderRadius:30,
-  
     alignItems:'center'
   
   },
 
   footer: {
     flex:1,
-    backgroundColor:'#2C3539',
+    backgroundColor:'green',
     width:'100%',
     height:'80%',
-    //marginBottom:4,
-    
     flexDirection: 'row',
     padding:10,
-    justifyContent: 'space-evenly'
+    justifyContent: 'space-between'
   },
 
   image:{  
     height: 80,
     width: 80,
-    borderRadius:50  
+    borderRadius:20 
     },
 
   text:{
--- Images.js
+++ Images.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Images = ({image}) => {
   return (
     <View style={styles.pic}>
-      <Image source={image} style={styles.images} resizeMode="cover"/>
+      <Image source={image} style={styles.images} resizeMode="resize"/>
     </View>
   )
 }
@@ -13,13 +13,9 @@ export default Images
 
 const styles = StyleSheet.create({
     images:{
-        height:'90%',
-        width:'90%',
-        borderRadius:20,
-        elevation:10,
-        borderColor:'gray',
-        borderBottomWidth:6,
-        borderLeftWidth:6
+        height:'100%',
+        width:'100%',
+        
     },
     pic:{
       width:'100%',
